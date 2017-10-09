0a8eb09b8117747d8184fe99d5843cf5ac16ece3
pxgamer <owzie123@gmail.com>
2017-10-09 13:27:55 +0100

    Added PhpDoc comments for MultiScraper class

--- src/MultiScraper/MultiScraper.php
+++ src/MultiScraper/MultiScraper.php
@@ -2,35 +2,81 @@
 
 namespace YeTii\MultiScraper;
 
+/**
+ * Class MultiScraper
+ */
 class MultiScraper
 {
+    /**
+     * @var bool
+     */
     private $debug = true;
+    /**
+     * @var array
+     */
     protected $sites = [];
+    /**
+     * @var null
+     */
     protected $user = null;
+    /**
+     * @var null
+     */
     protected $query = null;
+    /**
+     * @var int
+     */
     protected $page = 1;
+    /**
+     * @var array
+     */
     protected $torrents = [];
 
+    /**
+     * MultiScraper constructor.
+     * @param array|null $args
+     */
     public function __construct(array $args = null)
     {
 
     }
 
+    /**
+     * Get an attribute
+     *
+     * @param string $name
+     * @return null|mixed
+     */
     public function __get(string $name)
     {
         return isset($this->{$name}) ? $this->{$name} : null;
     }
 
+    /**
+     * Set an attribute
+     *
+     * @param string $name
+     * @param mixed  $value
+     */
     public function __set(string $name, $value)
     {
         $this->{$name} = $value;
     }
 
+    /**
+     * Initialise the sites
+     */
     public function initalize()
     {
         $this->sites = require __DIR__ . '/scrapers.php';
     }
 
+    /**
+     * Get the latest torrents from a site
+     *
+     * @param int $page
+     * @return array
+     */
     public function latest($page = 1)
     {
         if (!$this->initalized) {
@@ -41,6 +87,13 @@ class MultiScraper
         return $this->scrape();
     }
 
+    /**
+     * Get torrents that match a search query
+     *
+     * @param string $query
+     * @param int    $page
+     * @return array
+     */
     public function search($query, $page = 1)
     {
         if (!$this->initalized) {
@@ -52,6 +105,13 @@ class MultiScraper
         return $this->scrape();
     }
 
+    /**
+     * Get torrents that match a username
+     *
+     * @param string $user
+     * @param int    $page
+     * @return array
+     */
     public function user($user, $page = 1)
     {
         if (!$this->initalized) {
@@ -63,6 +123,11 @@ class MultiScraper
         return $this->scrape();
     }
 
+    /**
+     * Scrape a page for torrent data
+     *
+     * @return array|bool
+     */
     public function scrape()
     {
         $all = [];
