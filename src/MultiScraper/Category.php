dabb50e5bd502d78d5c360a9c394378a5be43da4
pxgamer <owzie123@gmail.com>
2017-10-13 12:24:05 +0100

    Added Category class

---
+++ src/MultiScraper/Category.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace YeTii\MultiScraper;
+
+/**
+ * Class Category
+ */
+class Category
+{
+    public const MOVIES = 1;
+    public const TV = 2;
+    public const GAMES = 3;
+    public const MUSIC = 4;
+    public const APPLICATIONS = 5;
+    public const BOOKS = 6;
+    public const XXX = 7;
+    public const OTHER = 8;
+}
